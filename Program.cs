3e4798877fbc9d43a2c1c037da0cc3a2ce77a2b8
guilhermesabreu <guilhermeabreucc@gmail.com>
2020-05-20 17:03:45 -0300

    Testes Paralelismo, finalizado

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            //Fazendo a Chamada paralela, cada um em sua Thread 
+            Parallel.Invoke( () => ImprimirZero(), () => ImprimirUm(), ()=> ImprimirZero());
+            stopwatch.Stop();
+            Console.WriteLine( );
+            System.Console.WriteLine("Parallel.Invoke -> A Tarefa foi finalizada em: {0}", stopwatch.ElapsedMilliseconds / 1000.0 + " segundos");
+            
+            stopwatch.Reset();
+            stopwatch.Start();
+            ImprimirZero();
+            ImprimirUm();
+            ImprimirPonto();
+            stopwatch.Stop();
+            Console.WriteLine();
+            System.Console.WriteLine("Sem Parallel.Invoke -> A Tarefa foi finalizada em: {0}", stopwatch.ElapsedMilliseconds / 1000.0 + " segundos");
+        }
+
+        private static void ImprimirPonto()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                ExecutarTarefaDemorada();
+                Console.Write(".");
+            } 
+        }
+
+        private static void ImprimirUm()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                ExecutarTarefaDemorada();
+                Console.Write("1");
+            } 
+        }
+
+        private static void ImprimirZero()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                ExecutarTarefaDemorada();
+                Console.Write("0");
+            } 
+        }
+
+
+        private static void ExecutarTarefaDemorada()
+        {
+            Thread.Sleep(10);
+        }
+    }
+}
